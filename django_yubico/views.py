f0772b2796f8965987504a3cfedd80fb0f6b7eeb
Tomaz Muraus <tomaz@tomaz.me>
2013-10-20 17:34:24 +0200

    Less copy and paste, reuse more code.

--- django_yubico/views.py
+++ django_yubico/views.py
@@ -24,11 +24,16 @@ YUBIKEY_SESSION_AUTH_BACKEND = getattr(settings, 'YUBICO_SESSION_AUTH_BACKEND',
                                        'yubicodjango_auth_backend')
 
 # Name of the session key which stores attempt counter
-YUBIKEY_ATTEMPT_COUNTER = getattr(settings, 'YUBIKEY_ATTEMPT_COUNTER',
-                                  'yubicodjango_counter')
+YUBIKEY_SESSION_ATTEMPT_COUNTER = getattr(settings,
+                                          'YUBIKEY_SESSION_ATTEMPT_COUNTER',
+                                          'yubicodjango_counter')
 
 # Name of the session key which stores number of password attemps
 YUBIKEY_PASSWORD_ATTEMPTS = getattr(settings, 'YUBICO_PASSWORD_ATTEMPTS', 3)
+
+# Django Yubico session keys
+SESSION_KEYS = [YUBIKEY_SESSION_USER_ID, YUBIKEY_SESSION_AUTH_BACKEND,
+                YUBIKEY_SESSION_ATTEMPT_COUNTER]
 
 
 @never_cache
@@ -51,7 +56,7 @@ def login(request, template_name='django_yubico/login.html',
                 # enter his password
                 request.session[YUBIKEY_SESSION_USER_ID] = user.pk
                 request.session[YUBIKEY_SESSION_AUTH_BACKEND] = user.backend
-                request.session[YUBIKEY_ATTEMPT_COUNTER] = 1
+                request.session[YUBIKEY_SESSION_ATTEMPT_COUNTER] = 1
 
                 return HttpResponseRedirect(reverse('yubico_django_password'))
             else:
@@ -76,9 +81,12 @@ def password(request, template_name='django_yubico/password.html',
     redirect_to = request.REQUEST.get(redirect_field_name,
                                       settings.LOGIN_REDIRECT_URL)
 
-    if not request.session.get(YUBIKEY_SESSION_USER_ID) or \
-       not request.session.get(YUBIKEY_ATTEMPT_COUNTER):
-        return HttpResponseRedirect(reverse('yubico_django_login'))
+    for key in SESSION_KEYS:
+        # Make sure all the required session keys are present
+        value = request.session.get(key, None)
+
+        if value is None:
+            return HttpResponseRedirect(reverse('yubico_django_login'))
 
     user_id = request.session[YUBIKEY_SESSION_USER_ID]
     auth_backend = request.session[YUBIKEY_SESSION_AUTH_BACKEND]
@@ -92,28 +100,17 @@ def password(request, template_name='django_yubico/password.html',
 
         if form.is_valid():
             auth_login(request, user)
-
-            try:
-                del request.session[YUBIKEY_SESSION_USER_ID]
-            except KeyError:
-                pass
-
-            try:
-                del request.session[YUBIKEY_ATTEMPT_COUNTER]
-            except KeyError:
-                pass
-
+            reset_user_session(session=request.session)
             return HttpResponseRedirect(redirect_to)
         else:
             # Limit the number of password attempts per token
-            request.session[YUBIKEY_ATTEMPT_COUNTER] += 1
+            request.session[YUBIKEY_SESSION_ATTEMPT_COUNTER] += 1
 
-            if request.session[YUBIKEY_ATTEMPT_COUNTER] > \
+            if request.session[YUBIKEY_SESSION_ATTEMPT_COUNTER] > \
                YUBIKEY_PASSWORD_ATTEMPTS:
                 # Maximum number of attemps has been reached. Require user to
                 # start from scratch.
-                del request.session[YUBIKEY_SESSION_USER_ID]
-                del request.session[YUBIKEY_ATTEMPT_COUNTER]
+                reset_user_session(session=request.session)
                 return HttpResponseRedirect(reverse('yubico_django_login'))
     else:
         form = PasswordForm(user=user)
@@ -121,3 +118,14 @@ def password(request, template_name='django_yubico/password.html',
     dictionary = {'form': form, redirect_field_name: redirect_to}
     return render_to_response(template_name, dictionary,
                               context_instance=RequestContext(request))
+
+
+def reset_user_session(session):
+    """
+    Remove all the Django Yubico related keys from the provided session.
+    """
+    for key in SESSION_KEYS:
+        try:
+            del session[key]
+        except KeyError:
+            pass
